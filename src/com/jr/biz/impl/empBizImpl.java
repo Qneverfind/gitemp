bfa9aa18ec979ed2ff5830ab8de3de7d4f0cdbd5
liuhongliang <2583561676@qq.com>
2022-11-29 16:27:58 +0800

    完整的项目结构

--- src/com/jr/biz/impl/empBizImpl.java
+++ src/com/jr/biz/impl/empBizImpl.java
@@ -1,39 +1,41 @@
 package com.jr.biz.impl;
 
 import com.jr.biz.IEmpBiz;
+import com.jr.dao.impl.empDaoImpl;
 import com.jr.entry.Emp;
 
 import java.util.List;
 
 public class empBizImpl implements IEmpBiz {
+    empDaoImpl empDao =  new empDaoImpl();
     @Override
     public boolean login(Emp emp) {
-        return false;
+      return empDao.selectEmp(emp).size()>0?true:false;
     }
 
     @Override
     public boolean register(Emp emp) {
-   return  false;
+   return  empDao.insertEmp(emp)==1?true:false;
     }
 
     @Override
     public List<Emp> queryAll() {
-        return null;
+        return empDao.selectAll();
     }
 
     @Override
     public boolean removeEmp(int empno) {
-        return false;
+        return empDao.dropEmp(empno)==1?true:false;
     }
 
     @Override
     public boolean changeEmp(Emp emp) {
-        return false;
+        return empDao.updateEmp(emp)==1?true:false;
     }
 
     @Override
     public List<Emp> queryByNo(int empno) {
-        return null;
+        return empDao.selectByNo(empno);
     }
 
 
